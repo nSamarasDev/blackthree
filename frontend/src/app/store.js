49c6298be713bdbd97f05918313e1be420701159
Nicholas Samaras <n.samaras4@outlook.com>
2023-04-12 17:22:42 -0400

    update

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import alert from "../reducers/alert";
 import auth from "../reducers/auth";
+import contact from "../reducers/contact";
 
 export const store = configureStore({
   reducer: {
     alert,
     auth,
+    contact,
   },
 });
