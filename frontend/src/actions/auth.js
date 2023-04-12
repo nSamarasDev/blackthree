2fbcb67548122e48ce74e12e0e999051f345a4a8
Nicholas Samaras <n.samaras4@outlook.com>
2023-04-12 17:20:50 -0400

    update

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
     REGISTER_FAIL,
     USER_LOADED,
     AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
     LOGOUT,
     CLEAR_PROFILE,
     CLEAR_APPLICATION,
@@ -62,6 +64,38 @@ export const register =
       });
     }
   };
+
+  // Login User
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("/api/auth", body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+  } catch (error) {
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+};
 
   // Logout / clear profile
 export const logout = () => (dispatch) => {
