d02dcf8c1a0d41edf0153428e880f71ca11d6c79
Nicholas Samaras <n.samaras4@outlook.com>
2023-04-14 12:59:36 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/api/users", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/contact", require("./routes/api/contact"));
 app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/post", require("./routes/api/post"));
+app.use("/api/posts", require("./routes/api/post"));
 
 
 
