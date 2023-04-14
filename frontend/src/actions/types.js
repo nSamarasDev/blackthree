972851b150dc0f1d9c51f913e79406d746668543
Nicholas Samaras <n.samaras4@outlook.com>
2023-04-14 10:01:56 -0400

    update

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -18,4 +18,10 @@ export const LOGOUT = "LOGOUT";
 export const CLEAR_PROFILE = "CLEAR_PROFILE";
 export const CLEAR_APPLICATION = "CLEAR_APPLICATION";
 
+export const GET_PROFILE = "GET_PROFILE";
+export const GET_PROFILES = "GET_PROFILES";
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
+export const GET_REPOS = "GET_REPOS";
+export const PROFILE_ERROR = "PROFILE_ERROR";
+
 export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
